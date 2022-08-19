b361ca1513e376e10cd88b121ac1fbedd6d0a51b
AkuinAshura <samuel.louis.mason@gmail.com>
2022-08-19 19:53:26 +1200

    Beginning Work on BFS
    Completed code that will help in BFS and A*, which are the algorithms we have decided to implement. Currently working on BFS algorithm.

--- Code/Algorithm.c
+++ Code/Algorithm.c
@@ -1,11 +1,29 @@
 // Includes
-
 #include <stdio.h>
 #include <limits.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 // Unicode things
 #include <Windows.h>
+
+typedef struct Queue {
+    int start, end;
+    unsigned capacity;
+    int *array;
+} Queue;
+
+void push() {
+
+}
+
+void pop() {
+
+}
+
+void BFS() {
+
+}
 
 int main() {
 
@@ -26,7 +44,11 @@ int main() {
     char seq;
     SetConsoleOutputCP(CP_UTF8);
 
+    // Store the dimensions of the map so we can't go outside of it later on.
+    int xydim, ydim = 0;
+
     // Read characters from the file and print them until we reach EOF.
+    // This part is only for demonstration purposes, we don't need this in the final code.
     while (1) {
         if (feof(fp) != 0) {
             break;
@@ -35,22 +57,54 @@ int main() {
         // Use ASCII code for 1, not 1 itself.
         if (seq == 49) {
             printf("%s", "██");
+            xydim++;
         } else if (seq == 48) {
             printf("▒▒");
+            xydim++;
         } else if (seq == 10) {
             printf("\n");
+            ydim++;
         }
-        // printf("%c", seq);
     }
+
+    // Start reading the file from the top again.
+    rewind(fp);
+
+    // Re-read the characters from the file, but this time, store them inside an array we just initialised.
+    int array[ydim][xydim / ydim];
+    int countx, county = 0;
+    while (1) {
+        if (feof(fp) != 0) {
+            break;
+        }
+        seq = fgetc((FILE*)fp);
+        // Use ASCII code for 1, not 1 itself.
+        if (seq == 49) {
+            array[county][countx] = 1;
+            countx++;
+        } else if (seq == 48) {
+            array[county][countx] = 0;
+            countx++;
+        } else if (seq == 10) {
+            county++;
+        }
+        countx++;
+    }
+    // Now the array is stored in memory.
+
+    // Print dimension for debugging purpose.
+    printf("(Size of the array is %d)\n", xydim);
 
     // Close the stream.
     fclose(fp);
 
-    struct queue {
-        int grid[100];
-        int start;
-        int end;
-    }
+    // Set up the queue.
+    Queue queue;
+
+    queue.capacity = xydim;
+    queue.start = 0;
+    queue.end = queue.capacity - 1;
+    queue.array = (int *) malloc(queue.capacity);
 
     int x1; int y1; int x2; int y2;
 
@@ -67,7 +121,7 @@ int main() {
     printf("Please enter the y co-ordinate:\n");
     scanf("%d", &y2);
 
-    BFS(x1, y1, x2, y2);
+    //BFS(x1, y1, x2, y2);
 
     //█ is for the completed path...
 
